4165b20cf3d4db3374d9b7af602fe8b001f071da
Jasper <jave.ong4@yahoo.com>
2016-07-13 17:11:43 -0400

    Create validation for project deadline

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,4 +1,12 @@
 class Pledge < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
+
+  validate :is_project_available
+
+  def is_project_available
+    if self.project.deadline_past?
+      errors.add(:project, "The deadline has passed.")
+    end
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,10 @@ class Project < ActiveRecord::Base
   has_many :rewards
 
   accepts_nested_attributes_for :rewards
+
+  def deadline_past?
+    if date.past?
+      errors.add(:project, "The deadline has passed.")
+    end
+  end
 end
