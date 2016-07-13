5953c2d12ec8e35b432c8fdf36adac4ab32452c6
Hira Kazmi <hirarukh@gmail.com>
2016-07-13 13:41:34 -0400

    added name migration to reward, added new project and reward form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713033458) do
+ActiveRecord::Schema.define(version: 20160713172403) do
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "amount"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160713033458) do
     t.integer  "amountThreshold"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
