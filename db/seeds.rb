034b0f6e4c45d5946b4375a1a707f3328c8ca5f4
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-13 16:44:00 -0400

    Put destroy all at top of seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Project.destroy_all
+Reward.destroy_all
+
 
 User.create(name: 'Jim Bean',
 						email: 'jim_bean@gmail.com',
