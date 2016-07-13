d5f5853f80789886fbd1c9f7702498060e268119
Hira Kazmi <hirarukh@gmail.com>
2016-07-12 21:46:45 -0400

    error in routing, trying to fix

--- crowdfunder/app/controllers/projects_controller.rb
+++ crowdfunder/app/controllers/projects_controller.rb
@@ -1,34 +1,34 @@
 class ProjectsController < ApplicationController
 
-  def index
-    @projects = Project.all
-  end
-
-  def show
-    @project = Project.find(params[:id])
-  end
-
-  def new
-    @project = Project.new
-  end
-
-  def edit
-    @project = Project.find(params[:id])
-  end
-
-  def create
-    @project = Project.new(project_params)
-
-    if @project.save
-      redirect_to projects_url
-    else
-      render :new
-    end
-  end
-
-  private
-  def project_params
-    params.require(:project).permit(:name, :description, :goal, :date, :url)
-  end
+  # def index
+  #   @projects = Project.all
+  # end
+  #
+  # def show
+  #   @project = Project.find(params[:id])
+  # end
+  #
+  # def new
+  #   @project = Project.new
+  # end
+  #
+  # def edit
+  #   @project = Project.find(params[:id])
+  # end
+  #
+  # def create
+  #   @project = Project.new(project_params)
+  #
+  #   if @project.save
+  #     redirect_to projects_url
+  #   else
+  #     render :new
+  #   end
+  # end
+  #
+  # private
+  # def project_params
+  #   params.require(:project).permit(:name, :description, :goal, :date, :url)
+  # end
 
 end
