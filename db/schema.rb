690806f4ca3d0319f66418371cacce17c5512e30
Hira Kazmi <hirarukh@gmail.com>
2016-07-12 23:58:49 -0400

    added pasword digest to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713000834) do
+ActiveRecord::Schema.define(version: 20160713033458) do
 
   create_table "pledges", force: :cascade do |t|
     t.integer  "amount"
@@ -38,9 +38,9 @@ ActiveRecord::Schema.define(version: 20160713000834) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
