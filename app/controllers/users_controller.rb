038cd3d357b3e30ab26748d5ebdd5f7a5964caeb
Mitch Pateman <mitch_pateman@hotmail.com>
2016-07-14 18:06:43 -0400

    Fixed the Users Account page to show the projects they created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,26 @@
 class UsersController < ApplicationController
 
-  def new
-    @user = User.new
-  end
+	def new
+		@user = User.new
+	end
 
-  def show
-    @user = User.find(params[:id])
+	def show
+		@user = User.find(params[:id])
+		@projects = @user.projects
+	end
 
-  end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to projects_url, notice: "Signed up!"
+		else
+			render "new"
+		end
+	end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to projects_url, notice: "Signed up!"
-    else
-      render "new"
-    end
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+	private
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation, project_attributes: [:name, :description, :goal, :date, :url, :user_id])
+	end
 
 end
