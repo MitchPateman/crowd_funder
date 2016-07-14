8f2f7c1b303bff6ee07faf04825d2a292ae7e756
Jasper <jave.ong4@yahoo.com>
2016-07-14 19:31:25 -0400

    Made rewards show on profile page

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,2 +1,19 @@
 class RewardsController < ApplicationController
+  # def new
+  #   @reward = @project.rewards.build
+  # end
+  #
+  # def create
+  #   @reward = @project.rewards.build(reward_params)
+  #   if @reward.save
+  #     flash[:notice] = "Reward created!"
+  #   else
+  #     render :new
+  #   end
+  # end
+  #
+  # private
+  # def reward_params
+  #   params.require(:reward).permit(:description, :amountThreshold)
+  # end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,15 +12,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to projects_url, notice: "Signed up!"
-    else
-      render "new"
-    end
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
       session[:user_id] = @user.id
       redirect_to projects_url, notice: "Successfully signed up and logged in!"
     else
