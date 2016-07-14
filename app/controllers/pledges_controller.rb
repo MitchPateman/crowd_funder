aa4c43a73cb18cc214f3f274d68783b9ea0e56ba
Brian Harrison <brianharrison@rogers.com>
2016-07-14 15:54:45 -0400

    Updated pledge controller

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,2 +1,18 @@
 class PledgesController < ApplicationController
+
+  def new
+    @pledge = Pledge.new
+  end
+
+  def create
+    @pledge = Pledge.new(pledge_params)
+  end
+
+  private
+
+  def pledge_params
+    params.require(:pledge).permit(:amount)  
+  end  
+
 end
+
