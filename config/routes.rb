e2fa8b317fb543687a8071c7b78f661086c5b0ea
Jasper <jave.ong4@yahoo.com>
2016-07-14 15:54:36 -0400

    Remove routes for pledge and rewards

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   root 'projects#index'
   resources :users, only: [:show, :new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-  resources :rewards
-  resources :pledges
   resources :projects
 end
