0f51e7bdf5629b1160d8ac3b29f0308e0d12338b
Brian Harrison <brianharrison@rogers.com>
2016-07-14 17:17:29 -0400

    Pledges connected to user and project

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,11 +1,20 @@
 class PledgesController < ApplicationController
 
   def new
-    @pledge = Pledge.new
+    
+    @pledge = @project.pledges.build
   end
 
   def create
-    @pledge = Pledge.new(pledge_params)
+    @project = Project.find(params[:project_id])
+    @pledge = @project.pledges.build(pledge_params)
+    @pledge.user = current_user
+
+    if @pledge.save
+      redirect_to project_url(@pledge.project)
+    else
+      render :new
+    end
   end
 
   private
