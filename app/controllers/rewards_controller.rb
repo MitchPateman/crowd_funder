9113d137f79668b4172d21de3ace71562482ca9e
Jasper <jave.ong4@yahoo.com>
2016-07-14 22:36:06 -0400

    Create new reward view

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,19 +1,21 @@
 class RewardsController < ApplicationController
-  # def new
-  #   @reward = @project.rewards.build
-  # end
-  #
-  # def create
-  #   @reward = @project.rewards.build(reward_params)
-  #   if @reward.save
-  #     flash[:notice] = "Reward created!"
-  #   else
-  #     render :new
-  #   end
-  # end
-  #
-  # private
-  # def reward_params
-  #   params.require(:reward).permit(:description, :amountThreshold)
-  # end
+  def new
+    @project = Project.find(params[:project_id])
+    @reward = @project.rewards.build
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @reward = @project.rewards.build(reward_params)
+    if @reward.save
+      flash[:notice] = "Reward created!"
+    else
+      render :new
+    end
+  end
+
+  private
+  def reward_params
+    params.require(:reward).permit(:description, :amountThreshold)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'projects#index'
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-  resources :rewards
   resources :projects do
+    resources :rewards, only: [:new, :create]
     resources :pledges, only: [:new, :create]
   end
 end
