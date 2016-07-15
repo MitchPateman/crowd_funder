e1f80efb9e8ceb7b3ff00a99638312cb4380d11b
Jasper <jave.ong4@yahoo.com>
2016-07-14 20:11:39 -0400

    Create user edit page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'projects#index'
-  resources :users, only: [:show, :new, :create]
+  resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :rewards
   resources :projects do
